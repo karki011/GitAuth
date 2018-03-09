f1c93ef8718a3175176372553805fb94028da710
subash karki <karki011@gmail.com>
2018-03-09 08:12:22 -0600

    GitAuth

---
+++ GitAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitAuth.Startup))]
+namespace GitAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
